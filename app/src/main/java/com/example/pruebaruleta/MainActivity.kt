9b98a4055fd0d01b0db3d42361c576c129a8e194
danielrob1 <daniel.robles.camazon@iesjulianmarias.es>
2025-01-14 09:02:15 +0100

    a

--- app/src/main/java/com/example/pruebaruleta/MainActivity.kt
+++ app/src/main/java/com/example/pruebaruleta/MainActivity.kt
@@ -4,6 +4,7 @@ import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.animation.DecelerateInterpolator
 import android.widget.Button
+import android.widget.EditText
 import android.widget.GridLayout
 import android.widget.ImageView
 import android.widget.Toast
@@ -16,6 +17,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var ruleta: ImageView
     private lateinit var btnGirar: Button
     private lateinit var gridLayout: GridLayout
+    private lateinit var editTextText: EditText
+    private lateinit var button: Button
+
 
     // Valores posibles de la ruleta (números y "Jackpot")
     //private val valoresRuleta = listOf("Jackpot", "1", "2", "3", "4", "5", "6", "7", "8")
@@ -27,7 +31,15 @@ class MainActivity : AppCompatActivity() {
         ruleta = findViewById(R.id.ruleta)
         btnGirar = findViewById(R.id.btnGirar)
         gridLayout = findViewById(R.id.gridLayout)
+        editTextText = findViewById(R.id.editTextText)
+        button = findViewById(R.id.button)
         verificarEspacios()
+        button.setOnClickListener {
+            val letra = editTextText.text.toString().uppercase().firstOrNull()
+            if (letra != null) {
+                verificarLetra(letra)
+            }
+        }
 
         btnGirar.setOnClickListener {
             girarRuleta()
@@ -51,10 +63,10 @@ class MainActivity : AppCompatActivity() {
             val anguloResultado = anguloAleatorio % 360
             val resultado = obtenerResultado(anguloResultado)
             mostrarResultado(resultado)
-            verificarLetra('A')
         }
         //cargarLetras(frase)
     }
+
 
     // Función que devuelve el resultado según el ángulo final
     private fun obtenerResultado(angulo: Int): String {
@@ -96,6 +108,7 @@ class MainActivity : AppCompatActivity() {
         return when (letra) {
 
             'A' -> R.drawable.letra
+            'U'-> R.drawable.u
             //'B' -> R.drawable.letra_b
             //'C' -> R.drawable.letra_c
             //'D' -> R.drawable.letra_d
